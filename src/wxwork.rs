1dd35da3ac5fc65efdad07f6bee458f42afaf325
kakie <782922086@qq.com>
2023-08-31 08:55:41 +0800

    decryption_works test

--- src/wxwork.rs
+++ src/wxwork.rs
@@ -299,8 +299,24 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        // let result = add(2, 2);
-        // assert_eq!(result, 4);
+    fn encryption_works() {}
+
+    #[test]
+    fn decryption_works() {
+        let corp_id = "wx5823bf96d3bd56c7";
+        let token = "QDG6eK";
+        let enc_aes_key = "jWmYm7qr5nMoAUwZRjGtBxmz3KA1tkAj3ykkR6q2B2C";
+
+        let wxw = WxWork::new(corp_id, token, enc_aes_key).expect("should be a WxWork object");
+
+        let timestamp = "1409659813";
+        let nonce = "1372623149";
+        let signature = "477715d11cdb4164915debcba66cb864d751f3e6";
+        let msg_encrypt = "RypEvHKD8QQKFhvQ6QleEB4J58tiPdvo+rtK1I9qca6aM/wvqnLSV5zEPeusUiX5L5X/0lWfrf0QADHHhGd3QczcdCUpj911L3vg3W/sYYvuJTs3TUUkSUXxaccAS0qhxchrRYt66wiSpGLYL42aM6A8dTT+6k4aSknmPj48kzJs8qLjvd4Xgpue06DOdnLxAUHzM6+kDZ+HMZfJYuR+LtwGc2hgf5gsijff0ekUNXZiqATP7PF5mZxZ3Izoun1s4zG4LUMnvw2r+KqCKIw+3IQH03v+BCA9nMELNqbSf6tiWSrXJB3LAVGUcallcrw8V2t9EL4EhzJWrQUax5wLVMNS0+rUPA3k22Ncx4XXZS9o0MBH27Bo6BpNelZpS+/uh9KsNlY6bHCmJU9p8g7m3fVKn28H3KDYA5Pl/T8Z1ptDAVe0lXdQ2YoyyH2uyPIGHBZZIs2pDBS8R07+qN+E7Q==";
+
+        let s = wxw
+            .decrypt(timestamp, nonce, signature, msg_encrypt)
+            .expect("decrypt thing should be a string result");
+        assert_eq!(s, "<xml><ToUserName><![CDATA[wx5823bf96d3bd56c7]]></ToUserName>\n<FromUserName><![CDATA[mycreate]]></FromUserName>\n<CreateTime>1409659813</CreateTime>\n<MsgType><![CDATA[text]]></MsgType>\n<Content><![CDATA[hello]]></Content>\n<MsgId>4561255354251345929</MsgId>\n<AgentID>218</AgentID>\n</xml>");
     }
 }
